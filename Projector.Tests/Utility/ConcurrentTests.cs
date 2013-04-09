95b453d58de6fb0cbb0fddd23facc110ecf3bc38
Jeff Sharp <jeff.sharp@git>
2013-04-08 21:41:24 -0500

    Add Concurrent helpers

---
+++ Projector.Tests/Utility/ConcurrentTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Projector.Utility
+{
+    [TestFixture]
+    public class ConcurrentTests
+    {
+        private const  int    TaskCount = 16;
+        private static object sharedLock;
+        private static int    sharedLocation; 
+
+        [Test]
+        public void Ensure()
+        {
+            var location = null as object;
+            var results  = new object[TaskCount];
+
+            Parallel.For(0, TaskCount, i =>
+            {
+                results[i] = Concurrent.Ensure(ref location, new object());
+            });
+
+            Assert.That(location, Is.Not.Null);
+            Assert.That(results, Is.All.SameAs(location));
+        }
+
+        [Test]
+        public void Lock_Unlock()
+        {
+            Parallel.For(0, TaskCount, UseLock);
+        }
+
+        private static void UseLock(int i)
+        {
+            var token = Concurrent.Lock(ref sharedLock);
+            try
+            {
+                sharedLocation = i;
+                Thread.Yield();
+                Thread.MemoryBarrier();
+                Assert.That(sharedLocation, Is.EqualTo(i));
+            }
+            finally
+            {
+                Concurrent.Unlock(ref sharedLock, token);
+            }
+        }
+    }
+}
---
+++ Projector/Utility/Concurrent.cs
@@ -0,0 +1,29 @@
+﻿namespace Projector
+{
+    using System.Threading;
+
+    internal static class Concurrent
+    {
+        public static T Ensure<T>(ref T location, T value)
+            where T : class
+        {
+            return Interlocked.CompareExchange(ref location, value, null) ?? value;
+        }
+
+        // For rarely-used locks
+        public static object Lock(ref object location)
+        {
+            var token = new object();
+            token = Interlocked.CompareExchange(ref location, token, null) ?? token;
+            Monitor.Enter(token);
+            return token;
+        }
+
+        // For rarely-used locks
+        public static void Unlock(ref object location, object token)
+        {
+            location = null;
+            Monitor.Exit(token);
+        }
+    }
+}
